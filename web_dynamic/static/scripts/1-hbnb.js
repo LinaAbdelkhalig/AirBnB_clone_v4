cd8add9b600f91b10da4c7929b23642ebcbf787f
Lina Abdelkhalig <lelsammani0@gmai.com>
2024-08-11 05:46:49 -0700

    still on task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	// this is where we store the amnety id when box is checked
+	let checkedAmenitiesList = {};
+
+	// update the amenities list in the html
+	function updateAmenList()
+	{
+		let getAmenList = Object.values(checkedAmenitiesList).join(', ');
+		$('.amenities h4').text(getAmenList);
+	}
+
+	// grabbing the input checkbox 
+	const checkbox = $('.amenities input[type="checkbox"]');
+
+	// Listening for checked and unchecked boxes
+	$(checkbox).change(function() {
+		// assign variables to the id and name from <input>
+		let amenId = $(this).data('id');
+		let amenName = $(this).data('name');
+
+		// if the box is checked add to list
+		if ($(this).is(':checked')) {
+			checkedAmenitiesList[amenId] = amenName;
+		}
+		else {
+			delete checkedAmenitiesList[amenId];
+		}
+	}
+
+	// invoke the earlier func to make changes to html
+	updateAmenList();
+});
